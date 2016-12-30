e98409a45fdd305dc24babcd907daee2b9f77c38
Shishir Thapa <shishirthapa1@gmail.com>
2016-12-30 04:23:03 +0000

    destroy action created

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -93,4 +93,19 @@ RSpec.describe PostsController, type: :controller do
             expect(response).to render_template :show
         end
     end
+    
+    describe "DELETE#destroy" do
+        it "deletes the post" do
+            post = FactoryGirl.create(:post)
+            expect{
+            delete :destroy, id: post.id
+            }.to change(Post, :count ).by(-1)
+        end
+        
+        it "redirects to root path" do
+            post = FactoryGirl.create(:post)
+            delete :destroy, id: post.id
+            expect(response).to redirect_to root_path
+        end
+    end
 end
